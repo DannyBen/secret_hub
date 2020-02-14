138b40f40bc00ed4beefdd1ee1f37ecc856af06e
Danny Ben Shitrit <db@dannyben.com>
2020-02-14 19:20:34 +0000

    fix ci colsole columns

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,29 @@
+# Simplecov
 require 'simplecov'
 SimpleCov.start
 
+# Dependencies
 require 'rubygems'
 require 'bundler'
 Bundler.require :default, :development
 
+# Our gem
 require 'secret_hub'
 require 'secret_hub/cli'
+include SecretHub
+
+# Our spec helpers
 require_relative 'fake_public_key'
 require_relative 'spec_mixin'
-
-include SecretHub
 include SpecMixin
 
+# Use our mock server instead of the actual GitHub API
 ENV['SECRET_HUB_API_BASE'] = 'http://localhost:3000'
+begin
+  HTTParty.get 'http://localhost:3000'
+rescue Errno::ECONNREFUSED
+  abort "\n==> ERROR: Please start the mock server using `run mockserver`\n\n"
+end
 
 # Dummy secrets for testing
 ENV['GITHUB_ACCESS_TOKEN'] = 'who took my token?'
@@ -21,15 +31,13 @@ ENV['SECRET'] = 'there is no spoon'
 ENV['PASSWORD'] = 'there is no spoon'
 ENV['SECRET_KEY'] = 'there is no spoon'
 
+# Make some place for testing
 system 'mkdir tmp' unless Dir.exist? 'tmp'
 
-# We nened our server to test
-begin
-  HTTParty.get 'http://localhost:3000'
-rescue Errno::ECONNREFUSED
-  abort "\n==> ERROR: Please start the mock server using `run mockserver`\n\n"
-end
+# Ensure terminal width consistency across tests
+ENV['COLUMNS'] = 80
 
+# Configure rspec
 RSpec.configure do |c|
   c.fixtures_path = 'spec/approvals'
   c.include SpecMixin
