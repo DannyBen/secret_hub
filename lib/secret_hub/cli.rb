0b7914da48491b93e1494e41a3bfa2298d34613b
Danny Ben Shitrit <db@dannyben.com>
2020-02-20 06:31:10 +0000

    add help footer

--- lib/secret_hub/cli.rb
+++ lib/secret_hub/cli.rb
@@ -9,7 +9,8 @@ module SecretHub
   class CLI
     def self.router
       router = MisterBin::Runner.new version: VERSION,
-        header: "GitHub Secret Manager"
+        header: "GitHub Secret Manager",
+        footer: "Run !txtpur!secrethub COMMAND --help!txtrst! for command specific help"
 
       router.route 'list',   to: Commands::List
       router.route 'save',   to: Commands::Save
